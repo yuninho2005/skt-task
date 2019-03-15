97f40c8b65b802900395809547e32c2e750335a6
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-15 11:34:13 -0700

    Initial test for the product controller

---
+++ management-app/src/test/java/com/sdevelopment/skt/management/tests/unit/ProductControllerTest.java
@@ -0,0 +1,36 @@
+package com.sdevelopment.skt.management.tests.unit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    public void shouldRenderCreateProductPage throws Exception {
+        //when(productService.greet()).thenReturn("Hello Mock");
+        this.mockMvc.perform(get("/create-product")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Mock")));
+    }
+}
