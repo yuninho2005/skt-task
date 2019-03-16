cd15a6e90bded8f9a58e80f926898d61fb5e0eed
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-16 10:36:17 -0700

    Creating domain class and updating this module pom to package it as jar

---
+++ common-library/src/main/java/com/sdevelopment/skt/common/domain/Product.java
@@ -0,0 +1,31 @@
+package com.sdevelopment.skt.common.domain;
+
+public class Product {
+    private Integer id;
+    private String name;
+
+    public Product() {
+
+    }
+
+    public Product(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
