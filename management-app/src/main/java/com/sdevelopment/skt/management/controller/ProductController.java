488a54808b16ea24d0792a52afdad0f6fce2e4b6
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-16 09:08:52 -0700

    Implementing product management feature and making tests pass

--- management-app/src/main/java/com/sdevelopment/skt/management/controller/ProductController.java
+++ management-app/src/main/java/com/sdevelopment/skt/management/controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.sdevelopment.skt.management.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
 public class ProductController {
+
+    @RequestMapping("/create-product")
+    public String createProduct(Map<String, Object> model) {
+        return "newProduct";
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Map<String, Object> model) {
+        return "listProducts";
+    }
 }
