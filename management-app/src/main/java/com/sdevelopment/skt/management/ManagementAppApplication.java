2f5876ceec67af4c63b8a59cc8a2ae8232723911
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-19 08:48:59 -0700

    Excluding the data source from the management-app

--- management-app/src/main/java/com/sdevelopment/skt/management/ManagementAppApplication.java
+++ management-app/src/main/java/com/sdevelopment/skt/management/ManagementAppApplication.java
@@ -1,11 +1,14 @@
 package com.sdevelopment.skt.management;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ManagementAppApplication extends SpringBootServletInitializer {
 
     @Override
