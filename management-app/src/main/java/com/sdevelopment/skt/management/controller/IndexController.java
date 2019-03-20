c12a9c06c9fe74f992313925e26b0ccdb9ac1cb3
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-20 09:46:40 -0700

    Using ModelMap instead of a normal map as per PR review comment

--- management-app/src/main/java/com/sdevelopment/skt/management/controller/IndexController.java
+++ management-app/src/main/java/com/sdevelopment/skt/management/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.sdevelopment.skt.management.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -9,7 +10,7 @@ import java.util.Map;
 public class IndexController {
 
     @RequestMapping("/")
-    public String home(Map<String, Object> model) {
+    public String home(ModelMap model) {
         model.put("message", "Welcome to the Products Management Portal");
         return "index";
     }
