4f39aa1e97daea0961537c7f1b32d6cb84ef8fe5
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-20 21:13:01 -0700

    Converting received message from json to object

--- microservice-app/src/main/java/com/sdevelopment/skt/microservice/listener/ProductMessageListener.java
+++ microservice-app/src/main/java/com/sdevelopment/skt/microservice/listener/ProductMessageListener.java
@@ -1,9 +1,12 @@
 package com.sdevelopment.skt.microservice.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sdevelopment.skt.common.domain.Product;
 import com.sdevelopment.skt.microservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -18,8 +21,16 @@ public class ProductMessageListener {
 
 
     public void receiveMessage(Map<String, String> message) {
-        message.get("id"); // TODO Convert from json to object
-        Product product;
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonInString = message.get("id"); // TODO Research what should i ask for here to the map
+
+        Product product = null;
+        try {
+            product = mapper.readValue(jsonInString, Product.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         productService.saveProduct(product);
     }
