b5ff5bee0c1111c5c36b43ba887b5f10cd102f78
Yunior Alayo <yalayo@NSL-YALAYOs-MacBook-Pro.local>
2019-03-25 14:38:46 -0700

    Removing commented code and organizing imports

--- microservice-app/src/test/java/com/sdevelopment/skt/microservice/dao/ProductRepositoryIntegrationTest.java
+++ microservice-app/src/test/java/com/sdevelopment/skt/microservice/dao/ProductRepositoryIntegrationTest.java
@@ -3,36 +3,21 @@ package com.sdevelopment.skt.microservice.dao;
 import com.sdevelopment.skt.common.domain.Product;
 import com.sdevelopment.skt.microservice.dao.impl.ProductRepositoryImpl;
 import com.sdevelopment.skt.microservice.exception.DuplicatedProductEception;
-import com.sdevelopment.skt.microservice.service.ProductService;
-import com.sdevelopment.skt.microservice.service.impl.ProductServiceImpl;
-import org.hibernate.jpa.internal.EntityManagerImpl;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-//@DataJpaTest
 @ActiveProfiles("test")
 @SpringBootTest
 public class ProductRepositoryIntegrationTest {
@@ -46,9 +31,6 @@ public class ProductRepositoryIntegrationTest {
         }
 
     }
-
-    //@PersistenceContext
-    //private EntityManager entityManager;
 
     @Autowired
     private ProductRepository productRepository;
